240b9d39cea02be9d57462be2023b22b5c6ce8e0
Amber Goulding <gouldinga1@cardiff.ac.uk>
2026-09-05 18:30:41 +0100

    Further extend when the search tree frequently extends

--- src/search.cpp
+++ src/search.cpp
@@ -1305,7 +1305,8 @@ moves_loop:  // When in check, search starts here
         ss->extension = extension;
 
         // Further extend in cases where the search tree has repeatedly been singular
-        if (extension > 0 && (ss - 1)->extension + ss->extension >= 3)
+        if (extension > 0 && (ss - 1)->extension && (ss - 2)->extension
+            && (ss - 1)->extension + (ss - 2)->extension >= 3)
         {
             extension += 1;
         }
