471dff27c2c6839dcfc499148bc547de2ff4cf5b
Amber Goulding <gouldinga1@cardiff.ac.uk>
2026-09-08 11:16:18 +0100

    Change 3ext margin when compounding Bench: 2528927

--- src/search.cpp
+++ src/search.cpp
@@ -1262,10 +1262,10 @@ moves_loop:  // When in check, search starts here
 
                 int corrValAdj   = std::abs(correctionValue) / 198368;
                 int doubleMargin = -2 + 204 * PvNode - 152 * !ttCapture - corrValAdj
-                                 - 1175 * ttMoveHistory / 114178 - (ss->ply > rootDepth) * 38
-                                 - 15 * compoundingExtensions;
+                                 - 1175 * ttMoveHistory / 114178 - (ss->ply > rootDepth) * 38;
                 int tripleMargin = 70 + 279 * PvNode - 188 * !ttCapture + 81 * ss->ttPv - corrValAdj
-                                 - (ss->ply > rootDepth) * 43;
+                                 - (ss->ply > rootDepth) * 43
+                                 - 35 * compoundingExtensions;;
 
                 extension =
                   1 + (value < singularBeta - doubleMargin) + (value < singularBeta - tripleMargin);
